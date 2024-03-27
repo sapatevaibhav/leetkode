6009faba9cbc22d07ff9b2fa9c6a173d87ca8ec8
sapatevaibhav <sapatevaibhav@duck.com>
2024-03-27 12:15:56 +0530

    added splashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:leetkode/pages/home_page.dart';
+import 'package:leetkode/pages/splash_screeen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         darkTheme: darkTheme,
         debugShowCheckedModeBanner: false,
         title: 'LeetKode',
-        home: const HomePage(),
+        home: const SplashScreen(),
       ),
     );
   }
